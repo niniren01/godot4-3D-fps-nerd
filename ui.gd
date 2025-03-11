3a7d57ff50be558e49a1382cef4dabd312aac440
niniren01 <mgcbok@163.com>
2025-03-11 15:33:20 +0800

    switch weapon func

--- ui.gd
+++ ui.gd
@@ -2,23 +2,35 @@ extends CanvasLayer
 @onready var hand: AnimatedSprite2D = $Hand
 
 var current_weapon : String = "gun"
-var ammo := 2
+var ammo := 100
+
+func _ready() -> void:
+	hand.play(current_weapon + "_idle")
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("mouse_left"):
 		if current_weapon == "knife":
 			hand.play("knife_stab")
-		elif current_weapon == "gun":
+		else:
 			if ammo > 0:
-				hand.play("gun_shoot")
+				hand.play(current_weapon + "_shoot")
 				ammo -= 1
 			else:
 				current_weapon = "knife"
 				hand.play("knife_idle")
-
+	
+	switch_weapon()
 
 func _on_hand_animation_finished() -> void:
-	if current_weapon == "knife":
-			hand.play("knife_idle")
-	elif current_weapon == "gun":
-			hand.play("gun_idle")
+	hand.play(current_weapon + "_idle")
+
+
+func switch_weapon():
+	if Input.is_action_just_pressed("weapon_1"):
+		current_weapon = "knife"
+	elif Input.is_action_just_pressed("weapon_2"):
+		current_weapon = "gun"
+	elif Input.is_action_just_pressed("weapon_3"):
+		current_weapon = "machine_gun"
+	elif Input.is_action_just_pressed("weapon_4"):
+		current_weapon = "mini_gun"	
